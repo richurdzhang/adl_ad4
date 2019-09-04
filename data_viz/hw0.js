08cc6ccf9e3f97638f76005ca37cda394e69a3e2
richurdzhang <rz2345@columbia.edu>
2019-09-04 02:45:19 -0400

    js for hw0

---
+++ data_viz/hw0.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(400, 400);
+}
+function toRad(deg){
+  return deg * (PI/180);
+}
+function draw() {
+  background(220);
+  var s = second();
+  var m = minute();
+  var h = hour();
+  strokeWeight(2);
+  noFill();
+  if(s==0){
+    point(200, 320);
+  }else{
+    arc(200, 200, 240, 240, PI + HALF_PI, PI + HALF_PI + toRad(s*6), OPEN);
+  }
+  if(m==0){
+    point(200, 340);
+  }else{
+    arc(200, 200, 280, 280, PI + HALF_PI, PI + HALF_PI + toRad(m*6), OPEN);
+  }
+  if(h==0){
+    point(200, 360);
+  }else{
+    arc(200, 200, 320, 320, PI + HALF_PI, PI + HALF_PI + toRad(h*15), OPEN);
+  }
+}
