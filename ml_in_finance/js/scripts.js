2a05637785b63c6d093f11e9e4dc24002905c2c0
richurdzhang <rz2345@columbia.edu>
2020-08-16 08:04:27 -0700

    Update scripts.js

--- ml_in_finance/js/scripts.js
+++ ml_in_finance/js/scripts.js
@@ -51,7 +51,7 @@ function build_second_table(companies, nums){
 $(document).ready(function() {
     d3.csv("./output/cleaned_outputs.csv").then(function(data) {
       console.log(data.slice(0,5)); // [{"Hello": "world"}, …]
-      console.log("HI");
+      console.log("ohaiyo");
     });
     table1 = $('#table1').DataTable();
     table2 = $('#table2').DataTable();
